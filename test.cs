47779dbed528cec39e5e0221c4fe9c73191b0cb7
NIGHT THINKER <kavyashree1524@gmail.com>
2021-04-22 13:29:08 +0530

    first commit

---
+++ test.cs
@@ -0,0 +1,14 @@
+using System.IO;
+using System;
+
+class Program
+{
+    static void Main()
+   {
+      int a=7;
+      int b=8;
+      Console.WriteLine(a+b);
+        Console.WriteLine(a-b);
+          Console.WriteLine(a*b);
+       }
+}
